0340adea1c3573834f0bc21611e5866dfc97c7ca
Benjamin Vølund <au578358@uni.au.dk>
2022-10-20 22:50:51 +0200

    first commit

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="w-full h-40 flex justify-between p-8">
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
